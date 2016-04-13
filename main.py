aede391685eb6d86dfda6a1fbed5825a543d8dbe
Waldeilson Santos <wwesantos@gmail.com>
2016-04-13 18:37:55 -0300

    command line
    Running by command line

--- main.py
+++ main.py
@@ -4,31 +4,49 @@ from RuleSet import RuleSet
 from optparse import OptionParser
 import sys
 
-if __name__ == '__main__':
 
-    file = open("data/RulesArticle.txt","r")
-    #file = open("data/MedidasO.2-4.txt","r")
-    fileExit = open("data/RankedRules.txt","w")
+def ruleSetFromFile(fname):
 
-    #Reading the input file
-    print("Loading file...")
+    file_iterator = open(fname, 'r')
     ruleSet = RuleSet(None)
-    for line in file:
+    for line in file_iterator:
+        line = line.strip().rstrip('\n').rstrip('\r') #removing end of line
         atributes = line.split(';')
         id = atributes[0]
         rule = atributes[1]
-        measures = [float(atributes[i].rstrip('\n').rstrip('\r')) for i in range(2, atributes.__len__())]
+        measures = [float(atributes[i]) for i in range(2, atributes.__len__())]
         ruleSet.appendRule(AssociationRule(id, rule, measures))
+    return ruleSet
+
+if __name__ == '__main__':
+
+    optparser = OptionParser()
+    optparser.add_option('-f', '--inputFile',
+                         dest='input',
+                         help='filename containing csv',
+                         default=None)
+    (options, args) = optparser.parse_args()
+
+    ruleSet = RuleSet(None)
+    if options.input is not None:
+        print("Opening file "+options.input)
+        ruleSet = ruleSetFromFile(options.input)
+    else:
+        #file = open("data/RulesArticle.txt","r")
+        print('No dataset filename specified, system with exit\n')
+        sys.exit('System will exit')
+
+    fileExit = open("data/RankedRules.txt","w")
 
     #Ranking the rules
-    print("Creating rank...", end="")
+    print("Creating rank...")
     rankRule = RankRule(ruleSet)
     rankedRules = rankRule.getRankedRules()
 
     #Writing output
-    print("Creating results file...")
+    print("Creating results file ./data/RankedRules.txt...")
     for rule in rankedRules:
-        #fileExit.write(str(rank+1) + ";" + str(rule.joinBy(";")) + "\n")
         fileExit.write(rule.__str__()+"\n")
 
-    print("Program ended...")
+    print("File ./data/RankedRules.txt created.")
+    print("Program finalized.")
